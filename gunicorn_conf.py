fe1d90da4e2652e7ab43ea0fa83f8cfce5dda382
cdfmlr <cdfmlr@163.com>
2020-04-03 09:45:28 +0800

    production

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -7,10 +7,10 @@ import logging.handlers
 from logging.handlers import WatchedFileHandler
 
 bind = '127.0.0.1:8080'      # 绑定ip和端口号
-chdir = '/home/admin/ser/musharing_ser/'  # gunicorn要切换到的目的工作目录
+# chdir = './'  # gunicorn要切换到的目的工作目录
 timeout = 30      # 超时
 daemon = True	# 后台
-workers = 1    # 进程数
+workers = 2    # 进程数
 loglevel = 'info' # 日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
 access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'    #设置gunicorn访问日志格式，错误日志无法设置
 
